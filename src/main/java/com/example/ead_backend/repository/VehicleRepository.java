3a2d5739e7a28cac35f8cd1033083b4da4e6d088
kin-lgtm <143951269+kin-lgtm@users.noreply.github.com>
2025-11-03 19:43:59 +0530

    create repo

--- src/main/java/com/example/ead_backend/repository/VehicleRepository.java
+++ src/main/java/com/example/ead_backend/repository/VehicleRepository.java
@@ -1,10 +1,10 @@
 package com.example.ead_backend.repository;
 
-import com.example.ead_backend.model.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.ead_backend.model.entity.Vehicle;
+
 @Repository
-public interface VehicleRepository extends JpaRepository<Vehicle, String> {
-    
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 }
