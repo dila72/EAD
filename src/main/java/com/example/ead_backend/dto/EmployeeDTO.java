5b78848630602b11b422e472c121482ec680a220
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-11-03 22:23:27 +0530

    created EmployeeDTO and Employee Entity entirely with mentioning how tables are connected with Appoinment and Project Entity. in Appoinment and Project Entity, only id's and table connection with Employee were mentioned .

--- src/main/java/com/example/ead_backend/dto/EmployeeDTO.java
+++ src/main/java/com/example/ead_backend/dto/EmployeeDTO.java
@@ -1,5 +1,20 @@
 package com.example.ead_backend.dto;
 
-public class EmployeeDTO {
+import java.io.Serializable;
+import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class EmployeeDTO implements Serializable {
+    private String employeeId;
+    private String name;
+    private String email;
+    private String password;
+    private String specialization;
+    private LocalDate joinedDate;
 }
