879320f337bf0eb4f59582b76ed88d345cb02053
Navindu Prabath Jayalath <143951269+kin-lgtm@users.noreply.github.com>
2025-11-07 22:04:52 +0530

    Refactor AppointmentMapper by removing unused mappings

--- src/main/java/com/example/ead_backend/mapper/AppointmentMapper.java
+++ src/main/java/com/example/ead_backend/mapper/AppointmentMapper.java
@@ -2,7 +2,6 @@ package com.example.ead_backend.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.AfterMapping;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import com.example.ead_backend.dto.AppointmentDTO;
@@ -10,20 +9,8 @@ import com.example.ead_backend.model.entity.Appointment;
 
 @Mapper(componentModel = "spring")
 public interface AppointmentMapper {
-    @Mapping(target = "employeeId", source = "employee.id")
-    @Mapping(target = "employeeName", expression = "java(getEmployeeName(entity))")
     AppointmentDTO toDTO(Appointment entity);
-    
-    @Mapping(target = "employee", ignore = true)
-    @Mapping(target = "time", ignore = true)
     Appointment toEntity(AppointmentDTO dto);
-
-    default String getEmployeeName(Appointment entity) {
-        if (entity != null && entity.getEmployee() != null && entity.getEmployee().getUser() != null) {
-            return entity.getEmployee().getUser().getFirstName() + " " + entity.getEmployee().getUser().getLastName();
-        }
-        return null;
-    }
 
     @AfterMapping
     default void setLegacyTime(@MappingTarget Appointment entity, AppointmentDTO dto) {
